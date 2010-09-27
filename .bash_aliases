5e75b3aa0c5a2ceeb2e2f31236e09de1a5c01199
Paul Stadig <paul@stadig.name>
2010-09-27 10:40:03 -0400

    Updated emacsclient / emacs server integration.

--- .bash_aliases
+++ .bash_aliases
@@ -1,4 +1,4 @@
-alias ubertest="lein clean && lein deps && lein test"
 alias l="ls -1"
 alias ll="ls -la"
 alias gits="git status -s"
+alias emacs="/usr/bin/emacsclient -nw"
--- .profile.d/50editor.sh
+++ .profile.d/50editor.sh
@@ -1,2 +1,7 @@
-export EDITOR="emacsclient"
-export ALTERNATE_EDITOR="emacs"
+export VISUAL="/usr/bin/emacsclient -c"
+export EDITOR="/usr/bin/emacsclient -nw"
+export ALTERNATE_EDITOR="/usr/bin/emacs"
+
+if ! emacsclient -a /bin/false --eval "nil" 2>1 1>/dev/null; then
+    /usr/bin/emacs --daemon 2>1 1>/dev/null
+fi

31f2f625cb1a2fb4973ddddee2fb2f61e5e5f909
Paul Stadig <paul@stadig.name>
2011-07-25 16:05:50 -0400

    Update google calendar mode.

--- .conkeror/new-google-calendar.js
+++ .conkeror/new-google-calendar.js
@@ -1,73 +1,63 @@
 /**
  * (C) Copyright 2008 Nelson Elhage
- * (C) Copyright 2009 John J. Foerch
+ * (C) Copyright 2009-2010 John J. Foerch
  *
  * Use, modification, and distribution are subject to the terms specified in the
  * COPYING file.
- **/
+**/
 
 in_module(null);
 
 require("content-buffer.js");
 
-define_keymap("google_calendar_keymap");
+define_keymap("google_calendar_keymap", $display_name = "google-calendar");
 
+define_key(google_calendar_keymap, "c", "copy");
 define_key(google_calendar_keymap, "C-c c", null, $fallthrough);
-define_key(google_calendar_keymap, "c", "copy");//BAD
 
-// define_key(google_calendar_keymap, "/", null, $fallthrough);
-// define_key(google_calendar_keymap, "p", null, $fallthrough);
-// define_key(google_calendar_keymap, "j", null, $fallthrough);
-// define_key(google_calendar_keymap, "n", null, $fallthrough);
-// define_key(google_calendar_keymap, "k", null, $fallthrough);
-// define_key(google_calendar_keymap, "t", null, $fallthrough);
-// define_key(google_calendar_keymap, "C-c t", "follow-top");//BAD
+define_key(google_calendar_keymap, "/", null, $fallthrough);
+define_key(google_calendar_keymap, "p", null, $fallthrough);
+define_key(google_calendar_keymap, "j", null, $fallthrough);
+define_key(google_calendar_keymap, "n", null, $fallthrough);
+define_key(google_calendar_keymap, "k", null, $fallthrough);
+define_key(google_calendar_keymap, "t", "follow-top");
+define_key(google_calendar_keymap, "C-c t", null, $fallthrough);
 
-// define_key(google_calendar_keymap, "d", null, $fallthrough);
-// define_key(google_calendar_keymap, "x", null, $fallthrough);
-// define_key(google_calendar_keymap, "C-c x", "shell-command-on-file");//BAD
-// define_key(google_calendar_keymap, "w", null, $fallthrough);
-// define_key(google_calendar_keymap, "m", null, $fallthrough);
-// define_key(google_calendar_keymap, "a", null, $fallthrough);
-//define_key(google_calendar_keymap, "q", "kill-current-buffer");
-//define_key(google_calendar_keymap, "C-c q", null, $fallthrough);
-//define_key(google_calendar_keymap, "q", "kill-current-buffer");
-//define_key(google_calendar_keymap, "C-c q", "kill-current-buffer");
-// define_key(google_calendar_keymap, "s", null, $fallthrough);
-// define_key(google_calendar_keymap, "C-c s", "save");//BAD
-// define_key(google_calendar_keymap, "u", null, $fallthrough);
-// define_key(google_calendar_keymap, "C-c u", "up");//BAD
-// define_key(google_calendar_keymap, "return", null, $fallthrough);
-// define_key(google_calendar_keymap, "tab", null, $fallthrough);//PROBABLY BAD
-// define_key(google_calendar_keymap, "M-s", null, $fallthrough);
-// define_key(google_calendar_keymap, "escape", null, $fallthrough);
+define_key(google_calendar_keymap, "d", null, $fallthrough);
+define_key(google_calendar_keymap, "x", null, $fallthrough);
+define_key(google_calendar_keymap, "w", null, $fallthrough);
+define_key(google_calendar_keymap, "m", null, $fallthrough);
+define_key(google_calendar_keymap, "a", null, $fallthrough);
+define_key(google_calendar_keymap, "q", "kill-current-buffer");
+define_key(google_calendar_keymap, "C-c q", null, $fallthrough);
+define_key(google_calendar_keymap, "s", null, $fallthrough);
+define_key(google_calendar_keymap, "C-c s", "save");
+define_key(google_calendar_keymap, "u", null, $fallthrough);
+define_key(google_calendar_keymap, "return", null, $fallthrough);
+define_key(google_calendar_keymap, "tab", null, $fallthrough);//PROBABLY BAD
+define_key(google_calendar_keymap, "C-c M-s", null, $fallthrough);
+//define_key(google_calendar_keymap, "escape", null, $fallthrough);
 
-function google_calendar_modality (buffer, element) {
-  if (! buffer.input_mode)
-    buffer.keymaps.push(google_calendar_keymap);
-}
+var google_calendar_modality = {
+    normal: google_calendar_keymap
+};
+
 
 define_page_mode("google_calendar_mode",
-                 $display_name = "New Google Calendar",
-                 $enable = function (buffer) {
-                   add_hook.call(buffer, "buffer_dom_content_loaded_hook",
-                                 google_calendar_modality);
-                   add_hook.call(buffer, "unfocus_hook", google_calendar_modality);
-                   buffer.modalities.push(google_calendar_modality);
-                 },
-                 $disable = function (buffer) {
-                   remove_hook.call(buffer, "buffer_dom_content_loaded_hook",
-                                    google_calendar_modality);
-                   remove_hook.call(buffer, "unfocus_hook", google_calendar_modalityc);
-                   var i = buffer.modalities.indexOf(google_calendar_modality);
-                   if (i > -1)
-                     buffer.modalities.splice(i, 1);
-                 });
+    $display_name = "New Google Calendar",
+    $enable = function (buffer) {
+        buffer.content_modalities.push(google_calendar_modality);
+    },
+    $disable = function (buffer) {
+        var i = buffer.content_modalities.indexOf(google_calendar_modality);
+        if (i > -1)
+            buffer.content_modalities.splice(i, 1);
+    });
 
 let (re = build_url_regex($domain = "google",
                           $path   = "calendar/",
                           $allow_www = true)) {
-  auto_mode_list.push([re, google_calendar_mode]);
+    auto_mode_list.push([re, google_calendar_mode]);
 }
 
 provide("google-calendar");

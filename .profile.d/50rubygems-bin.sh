5ae2e779fa09c348491c3fef76f6505ac8ffa402
Paul Stadig <paul@stadig.name>
2011-10-07 17:30:38 +0000

    Fix some path order issues.

--- .profile.d/50rubygems-bin.sh
+++
@@ -1,4 +0,0 @@
-# set PATH so it includes system rubygems bin if it exists
-if [ -d "/var/lib/gems/1.8/bin" ] ; then
-    PATH="/var/lib/gems/1.8/bin:$PATH"
-fi
--- .profile.d/98home-bin.sh
+++ .profile.d/98home-bin.sh
@@ -1,9 +1,9 @@
-# set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
-fi
-
 # set PATH so it includes user's private rubygems bin if it exists
 if [ -d "$HOME/.gem/ruby/1.8/bin" ] ; then
     PATH="$HOME/.gem/ruby/1.8/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
 fi
